a09c5f9b68d86a225bf8dd04a9dbd340c3122fed
oscar <oscar@Granados>
2016-09-01 00:16:24 -0500

    new commit

---
+++ src/laboratorio1/Problema1a.java
@@ -0,0 +1,29 @@
+
+package laboratorio1;
+
+/**
+ *
+ * @author oscar
+ */
+public class Problema1a {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Primera funcion en Main");
+    	Problema1a p = new Problema1a();
+    	p.func1();
+    	p.func2();
+    	System.out.println("Se termina Main");
+    }
+    
+    void func1(){
+        	System.out.println("Segunda funcion");
+	}
+    
+    void func2(){
+        	System.out.println("Tercera funcion");
+	}
+
+    
+    
+}
