5371bb8ac0d7a69520ed111ee3396b9d48468c15
OscarGG13 <ojgranadosg@unal.edu.co>
2016-09-05 10:28:10 -0500

    New commit

--- src/laboratorio1/Problema1b.java
+++ src/laboratorio1/Problema1b.java
@@ -1,47 +1,49 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Universidad Nacional de Colombia- Bogotá
+ * Curso: Programacion Orientada a objetos 
+ * 	Adivinar numero aleatorio 
+ *
+ *@autor Oscar Granados, Core Pelayo, Jairo Luna
+ *@version 2.0 
+ *@since 29/08/2016
+ *
  */
 package laboratorio1;
 import java.util.*;
-/**
- *
- * @author oscar
- */
+
 class Problema1b{   
     
 	public static void main(String[] args) {
    	 
-    	System.out.println("Adivina el numero");
-    	System.out.println("Solo tienes 7 intentos");
-   	 
-   	 
+    	System.out.println("** ADIVINAR NUMERO **");
+    	System.out.println("Dispones de 7 intentos");
+    	
+    
     	String playAgain = "y";
-           	 
+    
     	while(true){
         	if(playAgain.charAt(0) != 'y' ){   	 
-            	System.out.println("Gracias por jugar");
-            	break;
+            		System.out.println("Gracias por jugar");
+            		break;
         	}else{
-            	GuessingGame();
-            	System.out.println("Quieres jugar de nuevo (y/n): ");
-            	Scanner l = new Scanner(System.in);
-            	playAgain = l.nextLine();
+            		GuessingGame();
+            		System.out.println("Jugar de nuevo? (y/n): ");
+            		Scanner l = new Scanner(System.in);
+            		playAgain = l.nextLine();
         	}
-    	}
+    	}//fin while 
    	 
 	}
 	public static void GuessingGame(){
-    	int answer = (int)Math.floor((Math.random()*100)+1);
-    	int guess = 0;
-    	int numGuesses = 1;
-    	System.out.println("Adivina un numero entre 0 y 100: ");
+    		int answer = (int)Math.floor((Math.random()*100)+1);
+    		int guess = 0;
+    		int numGuesses = 1;
+    		System.out.println("Adivina un numero entre 0 y 100: ");
    	 
     	while(true){
         	if(numGuesses>7 || guess==answer){
             	if(numGuesses>7){
-                	System.out.println("Tu perdiste");
+                	System.out.println("PIERDES");
                 	break;
             	}else{
                 	break;
@@ -51,14 +53,14 @@ class Problema1b{
             	Scanner i = new Scanner(System.in);
             	guess = i.nextInt();
             	if(guess<answer){
-                	System.out.println("Mayor");
+                	System.out.println("Mas alto");
                 	numGuesses = numGuesses+1;
             	}else{
                 	if(guess>answer){
-                    	System.out.println("Menor");
+                    	System.out.println("Mas bajo");
                     	numGuesses = numGuesses+1;
                 	}else{
-                    	System.out.println("Tu ganaste");
+                    	System.out.println("!! GANADOR ¡¡");
                     	numGuesses = numGuesses+1;
                 	}
             	}
