d654e9132a8160bc675910ecfa3e517df4846491
OscarGG13 <ojgranadosg@unal.edu.co>
2016-09-05 10:44:15 -0500

    New commit

--- src/laboratorio1/Problema2a.java
+++ src/laboratorio1/Problema2a.java
@@ -1,28 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package laboratorio1;
 
 /**
+ *Universidad Nacional de Colombia
+ * Programacion Orientada a objetos
+ * Juego numeros aleatorios
  *
- * @author oscar
+ * @author Oscar Granados, Core Pelayo, Jairo Luna
+ * @since 01/09/2016
+ * @version 2
  */
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Vaio
- */
 public class Problema2a{
     
 	public static void main(String[] args) {
@@ -32,23 +23,23 @@ public class Problema2a{
     
 	public static void Lotery(){
     	int[] random = new int[3];
-    	random[0] = (int)Math.floor((Math.random()*10));
-    	random[1] = (int)Math.floor((Math.random()*10));
-    	random[2] = (int)Math.floor((Math.random()*10));
+    		random[0] = (int)Math.floor((Math.random()*10));
+    		random[1] = (int)Math.floor((Math.random()*10));
+    		random[2] = (int)Math.floor((Math.random()*10));
    	 
-    	System.out.println("Juego de Loteria -- Tres cifras");
-    	System.out.println("Digita los numeros de acuerdo a las instrucciones en pantalla");
+    	System.out.println("** JUEGO LOTERIA **");
+    	System.out.println("Escribe tus numeros en orden");
     	int[] guess = new int[3];
-    	System.out.print("Digita el numero de la posicion 1: ");
+    	System.out.print("Digita el primer numero: ");
     	Scanner l = new Scanner(System.in);
     	guess[0] = l.nextInt();
-    	System.out.print("Digita el numero de la posicion 2: ");
+    	System.out.print("Digita el segundo numero: ");
     	guess[1] = l.nextInt();
-    	System.out.print("Digita el numero de la posicion 3: ");
+    	System.out.print("Digita el tercer numero: ");
     	guess[2] = l.nextInt();
    	 
-    	System.out.println("Los numeros que ingresaste en orden son: " + Arrays.toString(guess));
-    	System.out.println("Los numeros ganadores en orden son:" + Arrays.toString(random));	 
+    	System.out.println("Los numeros a participar son: " + Arrays.toString(guess));
+    	System.out.println("Los numeros ganadores son:" + Arrays.toString(random));	 
    	 
     	int match=0;
     	if(guess[0] == random[0] || guess[0] == random[1] || guess[0] == random[2]){
@@ -69,26 +60,26 @@ public class Problema2a{
     	int reward;
     	if(matchcount==1){
         	reward=10;
-        	System.out.println("Tuviste un acierto");
+        	System.out.println("Has Acertado un numero");
         	System.out.println("Ganaste: " + reward);
     	}else if(matchcount==2){
         	reward=100;
-        	System.out.println("Tuviste dos aciertos");
+        	System.out.println("Has Acertado dos numeros");
         	System.out.println("Ganaste: " + reward);
     	}else if(matchcount==3){
         	if(guess[0]==random[0] && guess[1]==random[1] && guess[2]==random[2]){
             	reward=1000000;
-            	System.out.println("Tuviste tres aciertos en orden exacto");
+            	System.out.println("Has Acertado todos los numeros en orden exacto");
             	System.out.println("Ganaste: " + reward);
         	}else{
             	reward=1000;
-            	System.out.println("Tuviste tres aciertos");
+            	System.out.println("Has Acertado tres numeros");
             	System.out.println("Ganaste: " + reward);
         	}
     	}else{
         	reward=0;
-        	System.out.println("No tuviste aciertos");
-        	System.out.println("No ganaste");
+        	System.out.println("Ningun acierto");
+        	System.out.println("PIERDES");
     	}
    	 
    	 
